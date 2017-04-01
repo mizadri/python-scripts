e7d4760e78c7a1a36d8337f15ae0702d8ba73a96
Adrian Garcia <adriagar@ucm.es>
2017-04-01 19:24:07 +0200

    Added parsing example script and parsing part to hist script

--- charts/hist.py
+++ charts/hist.py
@@ -2,10 +2,102 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn
+import sys
+import re
+import getopt
 
+#import seaborn
 #seaborn.set()
 
+# -p p1,p2,p3,..,pn : Patterns for each column ---> '#cccccc:/,...."
+# -y y-range: ej: 1:4.20 or 1:4.20:0.1 (Increment is at the end) ----> yticks_fixed=np.arange(0,1.1,0.1)
+def usage(help):
+	print """Usage: %s -i data_file [options]
+---Option List---
+-i input-file: CSV input data file.
+-d width,height : Dimensions.
+-c c1,c2,c3,..,cn : Idx Columns.
+-p p1,p2,p3,..,pn : Patterns for each column.
+-y y-range: ej: 1:4.20 or 1:4.20:0.1 (Increment is at the end).
+-P : enable percentage format on y axis.
+-l <ylabel>: set labels for y axis.
+-t <tag>: tag that will be appended to the filename of the figure.
+-r <offset_x>: Rotate x titles by 90 degrees and apply offset to separate them from the x axis.
+-n: no key.
+-f <fontspec>
+-R <row_selection>
+-A: compute average.
+-h : help"""% sys.argv[0]
+
+	if not help:
+		exit(1)
+	else:
+		exit(0)           
+
+try:
+		opts, args = getopt.getopt(sys.argv[1:], "i:d:c:p:y:kl:t:r:n:f:R:Ah", ["input-file","dimensions","columns","patterns","yrange","percentages","ylabels","tag","roffset","nokey","font","rows","average","help"])
+except getopt.GetoptError as err:
+	# print help information and exit:
+	print str(err) # will print something like "option -a not recognized"
+	usage(False)
+	sys.exit(2)
+
+	infile = None
+	figSize = [7.75,6.75]
+	columns = []
+	patterns = []
+	yrange = None
+	percentages = None
+	ylabels = []
+	tag = None
+	roffset = 0
+	nokey = False
+	font = "Helvetica"
+	rows = []
+	average = False
+
+	for o, arg in opts:
+		if o in ("-i", "--input"):
+			infile=arg 
+		elif o in ("-d", "--dimensions"):
+			figSize = [float(d) for d in arg.split(',')]
+		elif o in ("-c", "--columns"):
+			columns = arg.split(',')
+		elif o in ("-p", "--patterns"):
+			for coldesign in arg.split(','):
+				patterns.append(coldesign.split(':'))
+		elif o in ("-y", "--yrange"):
+			infile=yrange
+		elif o in ("-P", "--percentages"):
+			percentages=arg
+		elif o in ("-l", "--ylabels"):
+			ylabels=arg
+		elif o in ("-t", "--tag"):
+			tag=arg
+		elif o in ("-r", "--roffset"):
+			roffset=float(arg)
+		elif o in ("-n", "--nokey"):
+			nokey=True
+		elif o in ("-f", "--font"):
+			font=arg
+		elif o in ("-R", "--rows"):
+			rows=arg
+		elif o in ("-A", "--average"):
+			average=True
+		if o in ("-h", "--help"):
+			usage(True)
+			sys.exit()
+		else:
+			assert False, "unhandled option"
+
+
+if len(figSize)!=2:
+	print "The dimmension has to be expressed in the following format: 7.75,6.75"
+	exit(1)
+
+## MAIN
+
+plt.figure(figsize=figSize)
 
 tb=pd.read_excel("example.xlsx")
 
---
+++ charts/parse_args.py
@@ -0,0 +1,43 @@
+import sys
+import re
+import getopt
+
+def usage(help):
+	print "Usage: %s [ -m <metric> | -M <metric-file> | -b ]" % sys.argv[0]
+
+	if not help:
+		exit(1)
+	else:
+		exit(0) 
+
+# if an option is followed by ':' it needs a parameter
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hm:M:b", ["input","help","metric","metric-file","bypass","output","verbose","accum","csv"])
+except getopt.GetoptError as err:
+	# print help information and exit:
+	print str(err) # will print something like "option -a not recognized"
+	usage(False)
+	sys.exit(2)
+
+figSize = [2,2]
+expressions=[]    
+metricFile=None
+bypass=False
+
+for o, arg in opts:
+	if o in ("-h", "--help"):
+		usage(True)
+		sys.exit()
+	elif o in ("-m", "--metric"):
+		figSize = [float(d) for d in arg.split(',')]
+		if len(figSize)!=2:
+			print "The dimmension has to be expressed in this format: 'x,y'"
+			exit(1)
+	elif o in ("-M", "--metric-file"):
+		metricFile=arg
+	elif o in ("-b", "--bypass"):
+		bypass=True        	
+	else:
+		assert False, "unhandled option"
+
+print figSize
