974155da3c5bf26b6b4af14e11c5839fbf776b63
Adrian Garcia <adriagar@ucm.es>
2017-04-01 17:23:46 +0200

    Added documentation to script

--- workload-selection/pick_workloads.py
+++ workload-selection/pick_workloads.py
@@ -4,13 +4,14 @@ import numpy as np
 table=pd.read_excel("spec_juno.xlsx",sheetname="data")
 csv = pd.read_csv("ctimes-juno.csv")
 
+# Load benchmark execution time in LITTLE to a dictionary
 bench_time = {}
 for raw_row in csv.iterrows():
 	i = raw_row[0]
 	row=raw_row[1]
 	bench_time[row[0]] = row[2]/1000
 
-
+# Load workload composition and SF to dicitionaries
 bench_SF = {}
 bench_lights = {}
 workloads = []
@@ -28,17 +29,7 @@ for raw_row in table.iterrows():
 			bench_lights[name] = b.strip() 
 		workloads.append(benchs)
 
-# SF_variations = []
-# for w in workloads:
-# 	SF_var = 0
-# 	last_SF = 0
-# 	for benchmark in w:
-# 		curr_SF = bench_SF[benchmark]
-# 		if last_SF > 0:
-# 			SF_var += abs(curr_SF-last_SF)
-# 		last_SF = curr_SF
-# 	SF_variations.append(SF_var/len(w))
-
+# Calculate variance of SF in each workload
 SF_variations = []
 for w in workloads:
 	array = []
@@ -46,12 +37,13 @@ for w in workloads:
 		array.append(bench_SF[b])
 	SF_variations.append(np.var(array))
 
+# Sort the different workloads using SF variance
 wdf = pd.DataFrame()
 wdf["SF_var"] = SF_variations
 wdf["Workload"] = table.Workload
 sortedd = wdf.sort_values("SF_var", ascending=False)
 
-
+# Obtain the longest benchmark (execution time) for each workload and print the workloads ordered by SF variance
 for i in sortedd.index:
 	time = []
 	for benchmark in workloads[i]:
