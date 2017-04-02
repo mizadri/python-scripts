e8166b6a27b4ce01f64c1ffd3820604fcc43ee61
Adrian Garcia <adriagar@ucm.es>
2017-04-02 13:51:19 +0200

    Implemented a bunch of script options

--- charts/generate_bar_chart.py
+++ charts/generate_bar_chart.py
@@ -2,9 +2,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib 
+from matplotlib.markers import *
 import sys
 import re
 import getopt
+import os.path
 
 #import seaborn
 #seaborn.set()
@@ -14,19 +17,20 @@ import getopt
 def usage(help):
 	print """Usage: %s -i data_file [options]
 ---Option List---
--i input-file: CSV input data file.
--d width,height : Dimensions.
--c c1,c2,c3,..,cn : Idx Columns.
--p p1,p2,p3,..,pn : Patterns for each column.
--y y-range: ej: 1:4.20 or 1:4.20:0.1 (Increment is at the end).
--P : enable percentage format on y axis.
+-i <input-file>: Input data file(excel or csv).
+-s <sheet name>: Excel sheet to obtain the data from.
+-d <width,height>: Dimensions.
+-c <c1,c2,..,cn>: Column's names to remove.
+-p <p1,p2,..,pn> : Patterns for each column.
+-y <start,end[,step]>: ej: 1:4.20 or 1:4.20:0.1 (Increment is at the end).
+-P : Enable percentage format on y axis.
 -l <ylabel>: set labels for y axis.
--t <tag>: tag that will be appended to the filename of the figure.
+-t <tag>: Tag that will be appended to the filename of the figure.
 -r <offset_x>: Rotate x titles by 90 degrees and apply offset to separate them from the x axis.
--n: no key.
+-n : no key.
 -f <fontspec>
 -R <row_selection>
--A: compute average.
+-A : compute average.
 -h : help"""% sys.argv[0]
 
 	if not help:
@@ -35,81 +39,109 @@ def usage(help):
 		exit(0)           
 
 try:
-		opts, args = getopt.getopt(sys.argv[1:], "i:d:c:p:y:kl:t:r:n:f:R:Ah", ["input-file","dimensions","columns","patterns","yrange","percentages","ylabels","tag","roffset","nokey","font","rows","average","help"])
+	opts, args = getopt.getopt(sys.argv[1:], "i:s:d:c:p:y:kl:t:r:n:f:R:Ah",
+											 ["input-file","sheet","dimensions","columns","patterns"
+											 ,"yrange","percentages","ylabels","tag","roffset"
+											 ,"nokey","font","rows","average","help"])
 except getopt.GetoptError as err:
 	# print help information and exit:
 	print str(err) # will print something like "option -a not recognized"
 	usage(False)
 	sys.exit(2)
 
-	infile = None
-	figSize = [7.75,6.75]
-	columns = []
-	patterns = []
-	yrange = None
-	percentages = None
-	ylabels = []
-	tag = None
-	roffset = 0
-	nokey = False
-	font = "Helvetica"
-	rows = []
-	average = False
 
-	for o, arg in opts:
-		if o in ("-i", "--input"):
-			infile=arg 
-		elif o in ("-d", "--dimensions"):
-			figSize = [float(d) for d in arg.split(',')]
-		elif o in ("-c", "--columns"):
-			columns = arg.split(',')
-		elif o in ("-p", "--patterns"):
-			for coldesign in arg.split(','):
-				patterns.append(coldesign.split(':'))
-		elif o in ("-y", "--yrange"):
-			infile=yrange
-		elif o in ("-P", "--percentages"):
-			percentages=arg
-		elif o in ("-l", "--ylabels"):
-			ylabels=arg
-		elif o in ("-t", "--tag"):
-			tag=arg
-		elif o in ("-r", "--roffset"):
-			roffset=float(arg)
-		elif o in ("-n", "--nokey"):
-			nokey=True
-		elif o in ("-f", "--font"):
-			font=arg
-		elif o in ("-R", "--rows"):
-			rows=arg
-		elif o in ("-A", "--average"):
-			average=True
-		if o in ("-h", "--help"):
-			usage(True)
-			sys.exit()
-		else:
-			assert False, "unhandled option"
+infile = "example.xlsx"
+sheet = None
+figSize = [7.75,6.75]
+columns = None
+patterns = []
+yrange = None
+percentages = False
+ylabels = []
+tag = None
+roffset = 0
+nokey = False
+font = "Helvetica"
+rows = []
+average = False
 
+ystart = None
+yend = None
+ystep = None
 
-if len(figSize)!=2:
-	print "The dimmension has to be expressed in the following format: 7.75,6.75"
-	exit(1)
+for o, arg in opts:
+	if o in ("-i", "--input"):
+		infile=arg 
+		assert os.path.isfile(infilen), "Specify an existing input file: -i example.xlsx" 
+	if o in ("-s", "--sheet"):
+		sheet=arg 
+	elif o in ("-d", "--dimensions"):
+		figSize = [float(d) for d in arg.split(',')]
+		assert len(figSize)==2, "The dimmension has to be expressed in the following format: -d 7.75,6.75"
+	elif o in ("-c", "--columns"):
+		columns = arg.split(',')
+	elif o in ("-p", "--patterns"):
+		for coldesign in arg.split(','):
+			patterns.append(coldesign.split(':'))
+	elif o in ("-y", "--yrange"):
+		infile=yrange
+		yrange = arg.split(":")
+		assert len(yrange)==2 or len(yrange)==3, "yrange should have this format: 1:4.20 or 1:4.20:0.1"
+		ystart = yrange[0]
+		yend = yrange[1]
+		ystep = yrange[2]
+	elif o in ("-P", "--percentages"):
+		percentages=True
+	elif o in ("-l", "--ylabels"):
+		ylabels=arg
+	elif o in ("-t", "--tag"):
+		tag=arg
+	elif o in ("-r", "--roffset"):
+		roffset=float(arg)
+	elif o in ("-n", "--nokey"):
+		nokey=True
+	elif o in ("-f", "--font"):
+		font=arg
+	elif o in ("-R", "--rows"):
+		rows=arg
+	elif o in ("-A", "--average"):
+		average=True
+	if o in ("-h", "--help"):
+		usage(True)
+		sys.exit()
+	else:
+		assert False, "unhandled option"
 
-## MAIN
+if ".csv" in infile:
+	table=pd.read_csv(infile)
+else:
+	if sheet:
+		table=pd.read_excel(infile, sheetname=sheet)
+	else:
+		table=pd.read_excel(infile)
 
+nCols = len(table.columns) - 1
+
+# Create the plot
+ax=table.plot(kind='bar')
 plt.figure(figsize=figSize)
 
-tb=pd.read_excel("example.xlsx")
+# Filter columbs by name
+if columns:
+	table.drop(columns,axis=1,inplace=True)
+	nCols -= len(columns)
 
-# Create the basic plot with pandas default colors and everything ... (Make sure you pick the axes return object)
-#http://pandas.pydata.org/pandas-docs/version/0.17.0/generated/pandas.DataFrame.hist.html
-ax=tb.plot(kind='bar')
-# .hist() te lo hace directamente.
+if yrange:
+	plt.yticks(np.arrange(ystart,yend,ystep))
+
+
+
+# vals = ax.get_yticks()
+# ax.set_yticklabels(['{:3.2f}%'.format(x) for x in np.linspace(0,100,len(vals))])
 
 #Set labels using column #0 (The rotation is important)
-ax.set_xticklabels(tb.ix[:,0].values,rotation=0)
+ax.set_xticklabels(table.ix[:,0].values,rotation=0)
 
-#Set hatches and/or colors as desired in bars ...
 
 bars=ax.patches
 patterns = ('-', 'x', '\\', '\\\\', '.', '/','//','///')
@@ -128,10 +160,9 @@ designs['sevillana'] = '.'
 # And you can combine them
 designs['mix']='\/'
 
-nCols = len(tb.columns) - 1
-i = 0
-colPadding = 0.02
-accPadding = colPadding
+
+
+#Set hatches and/or colors as desired in bars ...
 arr = []
 for bar in bars:
 	w = bar.get_width()
@@ -143,9 +174,7 @@ for bar in bars:
 
 
 ##Don't forget to update the legend  to reflect the changes
-ax.legend(loc='upper center', ncol=3) 
-plt.tight_layout(h_pad=10)
-
+ax.legend(loc='upper center', ncol=nCols) 
 
 #plt.axhspan(ymin, ymax)			
 plt.show()
